58c7453e2ca8bedab69a05cd5f6d8c68244fdd4e
Aditya Tanikanti <aditya.tanikanti@gmail.com>
2023-06-14 02:03:24 +0000

    Codes to run cosmic tagger using tensorflow on Polaris

--- containers/datascience/tensorflow/Polaris/job_submission.sh
+++ containers/datascience/tensorflow/Polaris/job_submission.sh
@@ -31,11 +31,11 @@ echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_GPUS= ${NGPUS} GPUS_PER_NODE= ${NGPU_PER
 
 # Openmpi Does not work on Polaris
 # Test MPICH 
-echo C++ MPI
-mpiexec -hostfile $PBS_NODEFILE -n $NGPUS -ppn $NGPU_PER_NODE singularity exec -B /opt/nvidia -B /var/run/palsd/ -B /opt/cray/pe -B /opt/cray/libfabric $CONTAINER /usr/source/mpi_hello_world
+# echo C++ MPI
+# mpiexec -hostfile $PBS_NODEFILE -n $NGPUS -ppn $NGPU_PER_NODE singularity exec -B /opt/nvidia -B /var/run/palsd/ -B /opt/cray/pe -B /opt/cray/libfabric $CONTAINER /usr/source/mpi_hello_world
 
-echo Python MPI
-mpiexec -hostfile $PBS_NODEFILE -n $NGPUS -ppn $NGPU_PER_NODE singularity exec -B /opt/nvidia -B /var/run/palsd/ -B /opt/cray/pe -B /opt/cray/libfabric $CONTAINER python3 /usr/source/mpi_hello_world.py
+#echo Python MPI
+# mpiexec -hostfile $PBS_NODEFILE -n $NGPUS -ppn $NGPU_PER_NODE singularity exec -B /opt/nvidia -B /var/run/palsd/ -B /opt/cray/pe -B /opt/cray/libfabric $CONTAINER python3 /usr/source/mpi_hello_world.py
 
 # Run Cosmic Tagger
 #git clone https://github.com/coreyjadams/CosmicTagger.git
@@ -45,10 +45,10 @@ mpiexec -hostfile $PBS_NODEFILE -n $NGPUS -ppn $NGPU_PER_NODE singularity exec -
 
 
 # CPU example with 16 ranks per node
-PPN=16
-PROCS=$((NNODES * PPN))
-echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${PROCS} RANKS_PER_NODE= ${PPN}"
+# PPN=16
+# PROCS=$((NNODES * PPN))
+# echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${PROCS} RANKS_PER_NODE= ${PPN}"
 
 # CPU Only with tensorflow
-mpiexec -hostfile $PBS_NODEFILE -n $PROCS -ppn $PPN singularity exec -B /opt/nvidia -B /var/run/palsd/ -B /opt/cray/pe -B /opt/cray/libfabric $CONTAINER python3 CosmicTagger/bin/exec.py --config-name a21 framework=tensorflow run.id=test-1 run.compute_mode=CPU run.distributed=True run.precision="float32" run.minibatch_size=2 run.iterations=20
+# mpiexec -hostfile $PBS_NODEFILE -n $PROCS -ppn $PPN singularity exec -B /opt/nvidia -B /var/run/palsd/ -B /opt/cray/pe -B /opt/cray/libfabric $CONTAINER python3 CosmicTagger/bin/exec.py --config-name a21 framework=tensorflow run.id=test-1 run.compute_mode=CPU run.distributed=True run.precision="float32" run.minibatch_size=2 run.iterations=20
 
